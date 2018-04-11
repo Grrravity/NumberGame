26e884c5777d82f3d57dde077b2e6c486184a998
Grrravity <julienne.prof@gmail.com>
2018-04-11 09:15:50 +0200

    Ajout du code de l'objet joueur

---
+++ src/Player.java
@@ -0,0 +1,45 @@
+public class Player {
+	private int scorePlayer_ = 0, scoreBasis_ = 0;
+	private String playerName_ = "Inconnu";
+
+public Player(String pNom, int pScr, int sBase) {
+	playerName_ = pNom;
+	scorePlayer_ = pScr;
+	scoreBasis_ = sBase;
+}
+	//return player's score
+public int getPlayerScore() {
+	return scorePlayer_;
+}
+	//return the point scored at the end of the round
+public int getScoreBase() {
+	return scoreBasis_;
+}
+	//return player name
+public String getPlayerName() {
+	return playerName_;
+}
+	// set player's score
+public void setPlayerScore(int pScr) {
+	scorePlayer_ = pScr;
+}
+	//set the score earn each round regarding the number of attempt
+public void setScoreBasis(int base) {
+	int basecalc = 0; 
+	if (base >= 10)
+		basecalc = 1;
+	else if (base < 10 && base > 5)
+		basecalc = 3;
+	else
+		basecalc = 5;
+	scoreBasis_ = basecalc;
+}
+public void setPlayerName(String pName) {
+	playerName_ = pName;
+}
+	//add point(s) to player
+public int addPlayerScore() {
+	scorePlayer_ =+ scoreBasis_;
+	return scorePlayer_;
+}
+}
